f71b9fcac9bd7d742e5f6bb3ed8d8f12af01dd9d
DallasGiles <105821196+DallasGiles@users.noreply.github.com>
2024-08-20 14:38:16 -0600

    unhard coded

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize('postgres', 'postgres', 'password', {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST || 'localhost',
   dialect: 'postgres',
   port: process.env.DB_PORT || 5432,
